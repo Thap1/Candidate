475ffdb792e22f664b12300a3c4db37ece3d76b0
User <User@DESKTOP-SH65DQA>
2018-12-27 23:37:26 +0700

    update

--- candidate/src/entity/ExperienceCandidate.java
+++ candidate/src/entity/ExperienceCandidate.java
@@ -1,5 +1,27 @@
 package entity;
 
-public class ExperienceCandidate {
-
+public class ExperienceCandidate extends Candidate{
+    String expInYear;
+    String proSkill;
+    /**
+     * 
+     */
+    public ExperienceCandidate() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    /**
+     * @param candidateID
+     * @param firstName
+     * @param birtDate
+     * @param address
+     * @param phone
+     * @param email
+     * @param candidateType
+     */
+    public ExperienceCandidate(int candidateID, String firstName, int birtDate, String address, int phone, String email,
+            String candidateType) {
+        super(candidateID, firstName, birtDate, address, phone, email, candidateType);
+        // TODO Auto-generated constructor stub
+    }
 }
--- candidate/src/entity/FresherCandidate.java
+++ candidate/src/entity/FresherCandidate.java
@@ -1,5 +1,66 @@
 package entity;
 
-public class FresherCandidate {
+public class FresherCandidate extends Candidate{
+    String graduationDate;
+    String graduationRank;
+    String education;
+    
+   
+    /**
+     * 
+     */
+    public FresherCandidate() {
+        super();
+    }
 
+  
+    /**
+     * @param candidateID
+     * @param firstName
+     * @param birtDate
+     * @param address
+     * @param phone
+     * @param email
+     * @param candidateType
+     * @param graduationDate
+     * @param graduationRank
+     * @param education
+     */
+    public FresherCandidate(int candidateID, String firstName, int birtDate, String address, int phone, String email,
+            String candidateType, String graduationDate, String graduationRank, String education) {
+        super(candidateID, firstName, birtDate, address, phone, email, candidateType);
+        this.graduationDate = graduationDate;
+        this.graduationRank = graduationRank;
+        this.education = education;
+    }
+
+
+    public void setGraduationDate(String graduationDate) {
+        this.graduationDate = graduationDate;
+    }
+
+    public String getGraduationRank() {
+        return graduationRank;
+    }
+
+    public void setGraduationRank(String graduationRank) {
+        this.graduationRank = graduationRank;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+
+
+    
+    @Override
+    public String toString() {
+        return "FresherCandidate [graduationDate=" + graduationDate + ", graduationRank=" + graduationRank
+                + ", education=" + education + "]";
+    }
 }
